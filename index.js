f612e3d34bbf483f15396276ecfa285df9f62fe2
sowmya <sowmyalikki@gmail.com>
2022-06-14 14:24:18 +0200

    as

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
+let result=document.getElementById("inputtext");
 
-let display = document.getElementById('display');
+let calculate=(number)=>{
+    result.value+=number;
 
-let buttons = Array.from(document.getElementsByClassName('button'));
+}
 
-buttons.map( button => {
-    button.addEventListener ('click', (e) => { 
-        switch(e.target.innertext){
-            case 'c':
-                display.innerText = '';
-                break;
-            case '←':
-            if(display.innerText){
-                display.innerText = display.innerText.slice(0, -1);
-             }
-             break;
-             case '=':
-                 display.innerText = eval(display.innerText);
-              default:
-                display.innerText += e.target.innerText;
-             
-    
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
     }
-});
-});
+    catch(err){
+        alert("Enter the valid Input");
+
+    }
+}
+function clr(){
+    result.value= "1";
+}
+
+function del(){
+    result.value=result.value.slice(0,-1);
+}
